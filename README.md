45800dcecf0e639a9c673c34c85b8afb8780b5b3
Francesco <framarin1996@gmail.com>
2022-07-08 17:27:56 +0200

    implement rename function

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ﻿# file_Manager.cpp
 
 use command line to pass file names that you want to sort out.
+Use flag -o to sort and -r to rename before the file names.
 
 -function sort_File() enumerate the files you passed in a progressive numerical order.
 { e.g. @shell ./file_Manager -o a b c
 	your files are renamed as 01) a ; 02) b ; 03) c}
 
--function rename_File() has to be implemented.
+-function rename_File() change spaces and hyphens with underscores and makes the name of the file camelCased 
+{ e.g. from “file-with-hyphens and spaces.txt” -→to “file_With_Hyphens_And_Spaces.txt”]
--- file_Manager.cpp
+++ file_Manager.cpp
@@ -2,6 +2,7 @@
 #include <cstdio> // i use rename()
 #include <cstring>
 #include <string>
+#include <cctype>
 using namespace std;
 
 const int MAX_CHAR = 1000;
@@ -15,8 +16,8 @@ int main (int argc, char* argv[]) {
     if (argc < 2) cout << "pass to this program command line argument, we use argv[]" << endl << "-o to sort, -r to rename" << endl;
     else {
         if (strcmp(string_flag, "-o") == 0) sort_File(argv, argc);
-        //if (strcmp(string_flag, "-r") == 0) rinomina(argv, (argc-1));
-        if (strcmp(string_flag, "-o") != 0 && strcmp(string_flag, "-r") == 0) cout << "use flag -o to sort or -r to rename" << endl;
+        if (strcmp(string_flag, "-r") == 0) rename_File(argv, argc);
+        if (strcmp(string_flag, "-o") != 0 && strcmp(string_flag, "-r") != 0) cout << "use flag -o to sort or -r to rename" << endl;
     }   
     return 0;
 }
@@ -31,5 +32,35 @@ void sort_File (char* file_List[], int n) {
         strcat(sorted_Name, ") ");
         strcat(sorted_Name, file_List[i]);
         rename(file_List[i], sorted_Name);
+    }
+}
+
+void replace_Hyphen (char file_Name[]) {
+    for(int i = 0; i < strlen(file_Name); i++) {
+        if (file_Name[i] == '-') file_Name[i] = '_';
+    }
+}
+void replace_Space (char file_Name[]) {
+    for(int i = 0; i < strlen(file_Name); i++) {
+        if (file_Name[i] == ' ') file_Name[i] = '_';
+    }
+}
+void make_Camel_Case (char file_Name[]) {
+    for(int i = 1; i < strlen(file_Name); i++) { 
+        if ( (file_Name[i-1] == '_') && (isupper(file_Name[i]) == 0) ) file_Name[i] = toupper(file_Name[i]);
+
+    }
+}
+void modify_File_Name (char file_Name[]) {
+    replace_Hyphen(file_Name);
+    replace_Space(file_Name);
+    make_Camel_Case(file_Name); 
+}
+void rename_File (char* file_list[], int n) {
+    char new_name[MAX_CHAR];
+    for ( int i = 2 ; i < n ; i++ ) {
+        strcpy(new_name, file_list[i]);
+        modify_File_Name(new_name);
+        rename(file_list[i], new_name);
     }
 }
